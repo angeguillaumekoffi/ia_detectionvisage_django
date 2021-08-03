e2ea95aabfe3d8b534abc4fc3bac288b14a216cc
angeguillaumekoffi <angekoffiguillaume@gmail.com>
2021-08-03 01:44:18 +0000

    Deuxieme commit

--- test_getsolupro/settings.py
+++ test_getsolupro/settings.py
@@ -10,7 +10,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-(+9phc&w*-no+%z-=6fgo-@7xma%8-$wwcg8uff8c6yizyvmtx'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['*']
 
